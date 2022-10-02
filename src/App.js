6714ff44c1452d4c163f4b5d28c1e014e8c971aa
Abbas Aleinzy | عباس العنزي <42795041+almgro7al3nzy@users.noreply.github.com>
2022-10-02 12:28:18 +0300

    55zxc

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import HomePage from './Pages/HomePage';
-import ChatPage from './Pages/ChatPage';
+import React from "react"; 
 
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route exact path='/' element={<HomePage />} />
-        <Route exact path='/chats' element={<ChatPage />} />
-      </Routes>
-    </div>
-  );
+import { BrowserRouter as Router, Route } from "react-router-dom"; 
+
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat"; 
+import "./main.css"
+
+import { positions, Provider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+
+const options = {
+    timeout: 5000,
+    position: positions.MIDDLE
+};
+
+const App = () => { 
+    
+    return ( 
+        <Provider template={AlertTemplate} {...options}>
+            <Router> 
+                <Route path="/" exact component={Join} />
+                <Route path="/join" exact component={Join} />
+                <Route path="/chat" exact component={Chat} /> 
+            </Router>
+        </Provider>
+    ); 
 }
 
-export default App;
+export default App; 
+
+/* User enters data in "/", that data is passed to "/chat" using query-strings" */ 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter } from 'react-router-dom';
-import ChatProvider from './context/chatProvider';
+import React from "react";
+import ReactDOM from "react-dom"; 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <ChatProvider>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </ChatProvider>
-  </BrowserRouter>,
-);
+import App from "./App";
+
+ReactDOM.render(<App/>,document.querySelector("#root"));
+
